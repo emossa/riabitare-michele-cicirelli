25806643ce7d554a5893cd98f1a086daf4b547ea
Emanuele Mossa <emanuelemossa@wsba1_l011.originenet.it>
2025-04-15 09:44:54 +0200

    lead conversion on whatsapp send

--- src/components/ui/chat-widget.tsx
+++ src/components/ui/chat-widget.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { trackFormSubmission } from "@/lib/utils";
 import dynamic from "next/dynamic";
 import React, { useEffect } from "react";
 import { FloatingWhatsApp } from "react-floating-whatsapp";
@@ -9,13 +10,28 @@ const ChatWidget = () => {
         <FloatingWhatsApp
             statusMessage="Online"
             chatMessage="Hai bisogno di aiuto?"
-            phoneNumber="393471294953"
+            phoneNumber="393298064862"
             accountName="Riabitare Ristrutturazioni"
             placeholder="Scrivi un messaggio..."
             buttonStyle={{
                 zIndex: 40,
             }}
             avatar="/favicon.jpg"
+            onSubmit={
+                (message) => {
+                    trackFormSubmission(
+                        {
+                            formId: "richiesta_preventivo_wa",
+                            formName: "Richiesta Preventivo WhatsApp",
+                            formCategory: "quote",
+                            formData: {
+                                message: message,
+                            },
+                            success: true,
+                        }
+                    )
+                }
+            }
         />
     );
 };
